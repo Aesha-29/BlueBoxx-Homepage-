c2d8973a2319267b3dc80945463237361a0615f3
Aesha-29 <aeshadedakiya@gmail.com>
2026-01-20 13:09:45 +0530

    Initial commit - Blueboxx homepage

--- script.js
+++ script.js
@@ -1,44 +1,7 @@
-const counters = document.querySelectorAll(".counter");
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const counter = entry.target;
-        const target = +counter.dataset.target;
-        let current = 0;
-        const increment = target / 120;
-
-        const update = () => {
-          current += increment;
-
-          if (current < target) {
-            counter.innerText = Math.floor(current) + "+";
-            requestAnimationFrame(update);
-          } else {
-            counter.innerText = target + "+";
-          }
-        };
-
-        update();
-        observer.unobserve(counter);
-      }
-    });
-  },
-  { threshold: 0.6 },
-);
-
-counters.forEach((counter) => observer.observe(counter));
-
-
-const slider = document.getElementById("slider");
-const next = document.getElementById("nextBtn");
-const prev = document.getElementById("prevBtn");
-
-next.addEventListener("click", () => {
-  slider.scrollLeft += 220;
-});
-
-prev.addEventListener("click", () => {
-  slider.scrollLeft -= 220;
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
 });
